0ce9d21948b614e2f770276a2a38ff46ceadedc1
Pat Jones <prjones4@buffalo.edu>
2018-07-12 15:29:26 -0400

    endpoint add

--- server/server.go
+++ server/server.go
@@ -106,6 +106,7 @@ func Start(conf Config) error {
 	router.HandleFunc("/v1/auth", authorizer.LoginHandler()).Methods("POST")
 	router.HandleFunc("/v1/cache", auth(getCache(core))).Methods("GET")
 	router.HandleFunc("/v1/cache/refresh", auth(refreshCache(core))).Methods("POST")
+	router.HandleFunc("/", webHandler).Methods("GET")
 
 	fmt.Fprintf(logWriter, "Beginning listening on port %d\n", conf.Server.Port)
 
